6369873456311e8012594899b75d0a7bc1570e53
mansouroussama <oussama123mn@gmail.com>
2023-04-17 02:26:30 +0200

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'normalize.css'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { persistor, store } from './store'
 import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux'
@@ -15,9 +15,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
    <React.StrictMode>
       <Provider store={store}>
          <PersistGate loading={<Loading/>} persistor={persistor}>
-            <BrowserRouter>
+            <HashRouter>
                <App/>
-            </BrowserRouter>
+            </HashRouter>
          </PersistGate>
       </Provider>
    </React.StrictMode>,
